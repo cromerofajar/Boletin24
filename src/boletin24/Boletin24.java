2074a740d050211338d77d35fdc4a9d929dcf088
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-03-27 09:56:15 +0100

    Boletin24

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,17 @@
+package boletin24;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Boletin24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos obx=new Metodos();
+        obx.crear();
+    }
+    
+}
