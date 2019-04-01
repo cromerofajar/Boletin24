be2bc3d47d86cb2a5d5bf6e619a041e6659e6fb0
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-04-01 11:07:49 +0200

    Paleta

--- src/boletin24/Boletin24.java
+++ src/boletin24/Boletin24.java
@@ -10,8 +10,10 @@ public class Boletin24 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Metodos obx=new Metodos();
-        obx.crear();
+//        Metodos obx=new Metodos();
+//        obx.crear();
+        Paleta obxPale=new Paleta();
+        obxPale.setVisible(true);
     }
     
 }
